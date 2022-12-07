01eb0b927420ea96bbc3a9a255469141f190ce93
Ubirajara Neves <ubirajara.neves@usp.br>
2022-12-07 10:31:44 -0300

    Evolução dos testes.

--- src/calcula-valor.js
+++ src/calcula-valor.js
@@ -6,12 +6,32 @@ function calcularMontante(capital, taxa, periodos) {
 
 function arredondar(valor) {
     const precisao = 100;
-    const valorArredondado = Math.round(valor * precisao) / precisao;
+    const valorArredondado =
+        Math.round((valor + Number.EPSILON) * precisao) / precisao;
 
     return valorArredondado;
+}
+
+function calcularPrestacoes(montante, numeroParcelas) {
+    const prestacaoBase = arredondar(montante / numeroParcelas);
+    const resultado = Array(numeroParcelas).fill(prestacaoBase);
+
+    let somaPrestacoes = resultado.reduce((a, t) => a + t);
+    let diferenca = montante - somaPrestacoes;
+    let i = 0;
+
+    while (diferenca != 0) {
+        resultado[i] += 0.01;
+        somaPrestacoes = resultado.reduce((a, t) => a + t);
+        diferenca = montante - somaPrestacoes;
+        i++;
+    }
+
+    return resultado;
 }
 
 module.exports = {
     calcularMontante,
     arredondar,
+    calcularPrestacoes,
 };
--- test/calcula-valor.test.js
+++ test/calcula-valor.test.js
@@ -23,4 +23,73 @@ describe('arredondar', () => {
         const resultado = calculaValor.arredondar(538.4453124999998);
         expect(resultado).toBe(538.45);
     });
+
+    // 1.005 -> Esperado: 1.01, obtido: 1.00
+    test('1.005 deve retornar 1.01', () => {
+        const resultado = calculaValor.arredondar(1.005);
+        expect(resultado).toBe(1.01);
+    });
+});
+
+describe('calcularPrestacoes', () => {
+    test('O número de parcelas é igual ao número de prestações', () => {
+        // Premissas
+        const numeroPrestacoes = 6;
+
+        // Operação
+        const prestacoes = calculaValor.calcularPrestacoes(
+            200,
+            numeroPrestacoes,
+        );
+
+        // Resultado ou comportamento esperado
+        expect(prestacoes.length).toBe(numeroPrestacoes);
+    });
+
+    test('Uma prestação, valor igual ao montante', () => {
+        // Premissas
+        const numeroPrestacoes = 1;
+
+        // Operação
+        const prestacoes = calculaValor.calcularPrestacoes(
+            50,
+            numeroPrestacoes,
+        );
+
+        // Resultado ou comportamento esperado
+        expect(prestacoes.length).toBe(numeroPrestacoes);
+        expect(prestacoes[0]).toBe(50);
+    });
+
+    test('Duas prestações, valor é igual à metade do montante', () => {
+        const numeroPrestacoes = 2;
+        const prestacoes = calculaValor.calcularPrestacoes(
+            50,
+            numeroPrestacoes,
+        );
+
+        expect(prestacoes.length).toBe(numeroPrestacoes);
+        expect(prestacoes[0]).toBe(25);
+        expect(prestacoes[1]).toBe(25);
+    });
+
+    test('Valor da soma das prestações deve ser igual ao montante com duas casas decimais', () => {
+        // Dado (given)
+        const numeroPrestacoes = 3;
+        const montante = 100;
+
+        // Quando (when)
+        const prestacoes = calculaValor.calcularPrestacoes(
+            montante,
+            numeroPrestacoes,
+        );
+
+        // Então (then)
+        expect(prestacoes.length).toBe(numeroPrestacoes);
+
+        const soma = calculaValor.arredondar(
+            prestacoes[0] + prestacoes[1] + prestacoes[2],
+        );
+        expect(soma).toBe(montante);
+    });
 });
